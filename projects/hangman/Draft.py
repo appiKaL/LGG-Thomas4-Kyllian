0524618b1568ecb1222c03c15dcc579e2025febd
Kyllian <kyllianculot44@gmail.com>
2024-06-17 15:45:12 +0200

    ssds

---
+++ projects/hangman/Draft.py
@@ -0,0 +1,13 @@
+def get_word() -> str :
+    word = input("Enter a word:")
+    return word
+def get_lives() -> int :
+    pass
+def get_guess(suggested_letters) -> list[str]:
+    pass
+def assess_guess(secret_word : str , guessed_letter : str , lives_left : int) -> int :
+    pass
+def display_word(secret_word : str, suggested_letters : list[str]) -> bool :
+    pass
+def main():
+    pass
